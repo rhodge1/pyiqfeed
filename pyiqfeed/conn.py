6aa0b3e7445e659ad686f88ec0085359f69dee3c
rober <robert.hodge@moonlight.co.uk>
2021-03-16 12:49:27 +0000

    ENH: date type fixes

--- pyiqfeed/conn.py
+++ pyiqfeed/conn.py
@@ -1690,7 +1690,7 @@ class HistoryConn(FeedConn):
     # Tick data is returned as a numpy array of this dtype. Note that
     # "tick-data" in IQFeed parlance means every trade with the latest top of
     # book quote at the time of the trade, NOT every quote and every trade.
-    tick_type = np.dtype([('date', 'U26'),
+    tick_type = np.dtype([('date', 'M8[us]'),
                           ('last', 'f8'), ('last_sz', 'i8'),
                           ('tot_vlm', 'i8'), ('bid', 'f8'), ('ask', 'f8'),
                           ('tick_id', 'i8'), ('last_type', 'U1'), ('mkt_ctr', 'i4'),
@@ -1706,7 +1706,7 @@ class HistoryConn(FeedConn):
                              ('cond4', 'u1')])
 
     # Bar data is returned as a numpy array of this type.
-    bar_type = np.dtype([('date', 'U26'),
+    bar_type = np.dtype([('date', 'M8[us]'),
                          ('open_p', 'f8'), ('high_p', 'f8'),
                          ('low_p', 'f8'), ('close_p', 'f8'),
                          ('tot_vlm', 'u8'), ('prd_vlm', 'u8'),
@@ -2035,32 +2035,32 @@ class HistoryConn(FeedConn):
         else:
             return data
 
-    def _read_bars(self, req_id: str) -> np.array:
-        """Get buffer for req_id and transform to a numpy array of bars."""
-        res = self._get_data_buf(req_id)
-        if res.failed:
-            return np.array([res.err_msg], dtype='object')
-        else:
-            data = np.empty(res.num_pts, HistoryConn.bar_type)
-            line_num = 0
-            while res.raw_data and (line_num < res.num_pts):
-                dl = res.raw_data.popleft()
-                (dt, tm) = fr.read_posix_ts(dl[1])
-                data[line_num]['date'] = dt
-                data[line_num]['time'] = tm
-                data[line_num]['high_p'] = np.float64(dl[2])
-                data[line_num]['low_p'] = np.float64(dl[3])
-                data[line_num]['open_p'] = np.float64(dl[4])
-                data[line_num]['close_p'] = np.float64(dl[5])
-                data[line_num]['tot_vlm'] = np.int64(dl[6])
-                data[line_num]['prd_vlm'] = np.int64(dl[7])
-                data[line_num]['num_trds'] = np.int64(dl[8])
-                line_num += 1
-                if line_num >= res.num_pts:
-                    assert len(res.raw_data) == 0
-                if len(res.raw_data) == 0:
-                    assert line_num >= res.num_pts
-            return data
+    # def _read_bars(self, req_id: str) -> np.array:
+    #     """Get buffer for req_id and transform to a numpy array of bars."""
+    #     res = self._get_data_buf(req_id)
+    #     if res.failed:
+    #         return np.array([res.err_msg], dtype='object')
+    #     else:
+    #         data = np.empty(res.num_pts, HistoryConn.bar_type)
+    #         line_num = 0
+    #         while res.raw_data and (line_num < res.num_pts):
+    #             dl = res.raw_data.popleft()
+    #             (dt, tm) = fr.read_posix_ts(dl[1])
+    #             data[line_num]['date'] = dt
+    #             data[line_num]['time'] = tm
+    #             data[line_num]['high_p'] = np.float64(dl[2])
+    #             data[line_num]['low_p'] = np.float64(dl[3])
+    #             data[line_num]['open_p'] = np.float64(dl[4])
+    #             data[line_num]['close_p'] = np.float64(dl[5])
+    #             data[line_num]['tot_vlm'] = np.int64(dl[6])
+    #             data[line_num]['prd_vlm'] = np.int64(dl[7])
+    #             data[line_num]['num_trds'] = np.int64(dl[8])
+    #             line_num += 1
+    #             if line_num >= res.num_pts:
+    #                 assert len(res.raw_data) == 0
+    #             if len(res.raw_data) == 0:
+    #                 assert line_num >= res.num_pts
+    #         return data
 
     def _read_bars_arctic(self, req_id: str) -> np.array:
         """Get buffer for req_id and transform to a numpy array of bars."""
@@ -3218,10 +3218,10 @@ class BarConn(FeedConn):
     port = FeedConn.deriv_port
 
     interval_data_type = np.dtype(
-        [('symbol', 'S64'), ('date', 'U26'),
+        [('symbol', 'U64'), ('date', 'M8[us]'),
          ('open_p', 'f8'), ('high_p', 'f8'), ('low_p', 'f8'),
-         ('close_p', 'f8'), ('tot_vlm', 'u8'), ('prd_vlm', 'u8'),
-         ('num_trds', 'u8')])
+         ('close_p', 'f8'), ('tot_vlm', 'i8'), ('prd_vlm', 'i8'),
+         ('num_trds', 'i8')])
 
     def __init__(self, name: str = "BarConn", host: str = host,
                  port: int = port):
